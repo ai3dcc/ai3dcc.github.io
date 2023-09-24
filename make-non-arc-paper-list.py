be30ccea16d829597daac116ed7f3a408945ae5f
Despoina Paschalidou <paschalidoud@gmail.com>
2023-09-24 09:56:59 -0700

    Add some more posters

--- make-non-arc-paper-list.py
+++ make-non-arc-paper-list.py
@@ -60,6 +60,7 @@ publications = [
         author_list("Lukas Höllein; Ang Cao; Andrew Owens; Justin Johnson; Matthias Niessner"),
         [   Link("Abstract", None, "We present Text2Room, a method for generating room-scale textured 3D meshes from a given text prompt as input. To this end, we leverage pre-trained 2D text-to-image models to synthesize a sequence of images from different poses. In order to lift these outputs into a consistent 3D scene representation, we combine monocular depth estimation with a text-conditioned inpainting model. The core idea of our approach is a tailored viewpoint selection such that the content of each image can be fused into a seamless, textured 3D mesh. More specifically, we propose a continuous alignment strategy that iteratively fuses scene frames with the existing geometry to create a seamless mesh. Unlike existing works that focus on generating single objects [56, 41] or zoom-out trajectories [18] from text, our method generates complete 3D scenes with multiple objects and explicit 3D geometry. We evaluate our approach using qualitative and quantitative metrics, demonstrating it as the first method to generate room-scale 3D geometry with compelling textures from only text as input.", None),
             Link("Paper", "https://arxiv.org/pdf/2303.11989.pdf", None, None),
+            Link("Poster", "posters/iccv23_poster_text2room.pdf", None, None),
         ]
     ),
 
@@ -207,6 +208,7 @@ publications = [
         author_list("Kim Youwang; Tae-Hyun Oh"),
         [   Link("Abstract", None, "We present TexAvatar, a text-driven human texture generation system for creative human avatar synthesis. Despite the huge progress in text-driven human avatar generation methods, modeling high-quality, efficient human appearance remains challenging. With our proposed neural re-parameterized texture optimization, TexAvatar generates a high-quality UV texture in 30 minutes, given only a text description. The generated UV texture can be easily superimposed on animatable human meshes without further processing. This is distinctive in that prior works generate volumetric textured avatars that require cumbersome rigging processes to animate. We demonstrate that TexAvatar produces human avatars with favorable quality, with faster speed, compared to recent competing methods.", None),
             Link("Paper", "/papers/0042_camready.pdf", None, None),
+            Link("Poster", "posters/0042_poster.pdf", None, None),
         ]
     ),
 
